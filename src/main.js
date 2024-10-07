71f12da84ba40a7812ef73662d95ed31211e2def
George Ibarra <gibarr13@stu.southtexascollege.edu>
2024-10-06 22:19:09 -0500

    final commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import '@/style.css'
 import router from '@/router'
 import App from '@/App.vue'
-import composables from './composables'
 
 createApp(App).use(router).mount('#app')
